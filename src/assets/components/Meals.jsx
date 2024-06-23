029b716a8fa26b9aa96cf3f846d084dbeb643c11
Petermwansa <mwansapeter800@gmail.com>
2024-06-24 00:59:22 +0300

    use the custom hook to fetch the data and handle the error

--- src/assets/components/Meals.jsx
+++ src/assets/components/Meals.jsx
@@ -1,29 +1,21 @@
-import React, { useEffect, useState } from 'react'
+import useHttp from '../hooks/useHttp';
 import MealItem from './MealItem';
+
+
+
+// we create this obj outside of the component so that it will not be recreated everytime the component is rendered to avoind an infinite loop
+//meaning, we are using the same obj in memory 
+const requestConfig = {};
 
 const Meals = () => {
 
-    const [loadedMeals, setLoadedMeals] = useState([]);
+  const {data: loadedMeals, isLoading, error } = useHttp('http://localhost:3000/meals', requestConfig, []);
 
 
-    // this hook will prevent us from running into an infinite loop 
-    useEffect(() => {
-        async function fetchMeals() {
-            const response = await fetch('http://localhost:3000/meals');
-    
-            if (!response.ok) {
-                
-            }
-    
-            const meals = await response.json();
-            setLoadedMeals(meals);
-        }
-
-        fetchMeals();
-    }, [])
-
-
-
+  if (isLoading) {
+    return <p>Fetching meals...</p>;
+  }
+  
 
   return (
     <ul id='meals'>
@@ -38,3 +30,47 @@ const Meals = () => {
 }
 
 export default Meals
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import MealItem from './MealItem.jsx';
+// import useHttp from '../hooks/useHttp.js';
+
+// const requestConfig = {};
+
+// export default function Meals() {
+//   const {
+//     data: loadedMeals,
+//     isLoading,
+//     error,
+//   } = useHttp('http://localhost:3000/meals', requestConfig, []);
+
+//   if (isLoading) {
+//     return <p>Fetching meals...</p>;
+//   }
+
+//   // if (!data) {
+//   //   return <p>No meals found.</p>
+//   // }
+
+//   return (
+//     <ul id="meals">
+//       {loadedMeals.map((meal) => (
+//         <MealItem key={meal.id} meal={meal} />
+//       ))}
+//     </ul>
+//   );
+// }
